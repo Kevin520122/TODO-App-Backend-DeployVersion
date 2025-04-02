9127b026aa68bab55fc1757d2d4263ea95ca903c
Kevin520122 <zhaoshilong4@gmail.com>
2025-04-02 11:10:19 -0700

    config origin

--- server.js
+++ server.js
@@ -1,41 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const connectDB = require('./config/db');
-// require('dotenv').config();
-
-// // Initialize Express
-// const app = express();
-
-// // Connect to Database
-// connectDB();
-
-// // Middleware
-
-// app.use(cors(
-// {
-//   origin: ['https://todo-app-digital-factory-git-main-kevinzhaos-projects-c96a4fb1.vercel.app', 'http://localhost:3001'],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true
-// }
-// ))
-// // app.use(cors());
-
-// app.use(express.json({ extended: false }));
-
-// // Define Routes
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/tasks', require('./routes/tasks'));
-
-// // Basic route
-// app.get('/', (req, res) => {
-//   res.json({ message: 'Welcome to the Todo API' });
-// });
-
-// // Define port
-// const PORT = process.env.PORT || 5000;
-
-// // Start server
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
@@ -60,9 +22,11 @@ connectDB();
 
 // CORS Middleware with flexible configuration
 app.use(cors({
-    origin: ['https://todo-app-digital-factory-git-main-kevinzhaos-projects-c96a4fb1.vercel.app', 'http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+    origin: [
+      'https://todo-app-digital-factory-git-main-kevinzhaos-projects-c96a4fb1.vercel.app', 
+      'https://todo-app-digital-factory-baafg3cfw-kevinzhaos-projects-c96a4fb1.vercel.app',
+      'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
